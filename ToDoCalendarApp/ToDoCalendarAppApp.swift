953e529c42705b9087e1f145f2016619e31bfdbe
Satoshi Kameyama <satoshikameyama@theurbanwriters.com>
2023-08-20 02:06:35 -0400

    Initial Commit

---
+++ ToDoCalendarApp/ToDoCalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoCalendarAppApp.swift
+//  ToDoCalendarApp
+//
+//  Created by Satoshi Kameyama on 2023-08-20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoCalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
